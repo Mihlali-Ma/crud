c00784399942cca681e20221aa852c1734adf94b
Imtiyaaz-D <imtdamon777@gmail.com>
2023-06-08 14:08:40 +0200

    editing

---
+++ js/code.js
@@ -0,0 +1,14 @@
+const posts = document.querySelector('posts');
+const 
+
+
+
+
+featuredPosts = [{
+    id:"",
+    title:"",
+    imageUrl:"",
+    description:"",
+    content:"",
+    date: newDate(),
+}]
